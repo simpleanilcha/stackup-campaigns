dec6f98e6785413ecb920794d9c56fd12d26a71a
Anil Maharjan <simpleanilcha@gmail.com>
2023-03-03 22:12:07 +0545

    first dapp: get quests information

--- my-first-dapp/frontend/src/App.js
+++ my-first-dapp/frontend/src/App.js
@@ -8,6 +8,7 @@ const abi = contract.abi;
 function App() {
  const [adminAddr, setAdminAddr] = useState("nil");
  const [currentAccount, setCurrentAccount] = useState(null);
+ const [allQuestsInfo, setAllQuestsInfo] = useState(null);
 
  const connectWalletHandler = async () => {
   if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
@@ -39,16 +40,56 @@ function App() {
   }
  };
 
+ const getQuestsInfo = async () => {
+  try {
+   const provider = new ethers.providers.Web3Provider(window.ethereum);
+   const stackupContract = new ethers.Contract(contractAddr, abi, provider);
+
+   const nextQuestId = await stackupContract.nextQuestId();
+   let allQuestsInfo = [];
+   let thisQuest;
+   for (let i = 0; i < nextQuestId; i++) {
+    thisQuest = await stackupContract.quests(i);
+    allQuestsInfo.push(thisQuest);
+   }
+   setAllQuestsInfo(allQuestsInfo);
+  } catch (err) {
+   console.log("getQuestsInfo error...");
+   console.log(err);
+  }
+ };
+
  useEffect(() => {
   getAdminAddr();
+  getQuestsInfo();
  });
 
  return (
   <div className="container">
    <h1>Build Your First Dapp</h1>
-   <h4>By: anilcha</h4>
+   <h4>By: REPLACE-WITH-YOUR-STACKUP-USERNAME</h4>
    {currentAccount ? <h4>Wallet connected: {currentAccount}</h4> : <button onClick={connectWalletHandler}>Connect Wallet</button>}
    <h4>Admin address: {adminAddr}</h4>
+   <h2>
+    <u>All Quests:</u>
+   </h2>
+
+   <div>
+    {allQuestsInfo &&
+     allQuestsInfo.map((quest) => {
+      return (
+       <div key={quest[0]}>
+        <h4>{quest[2]}</h4>
+        <ul>
+         <li>questId: {quest[0].toString()}</li>
+         <li>number of players: {quest[1].toString()}</li>
+         <li>reward: {quest[3].toString()}</li>
+         <li>number of rewards available: {quest[4].toString()}</li>
+        </ul>
+       </div>
+      );
+     })}
+   </div>
   </div>
  );
 }
