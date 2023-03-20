900e86c5e8e4e1ff9469e812aae1a4dceb6c4ab5
Anil Maharjan <simpleanilcha@gmail.com>
2023-03-20 23:19:02 +0545

    update: login screen with logo and form

--- app-development-react-native/react-login-app/login-app/App.js
+++ app-development-react-native/react-login-app/login-app/App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Image, View, TextInput, Button } from "react-native";
+import { users } from "./db";
 
 export default function App() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const handleClick = () => {
+    const user = users.find((u) => u.username === username && u.password === password);
+    if (user) {
+      alert("Login successful");
+    } else {
+      alert("Login failed");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/Header.jpg")} style={styles.image} />
+      <TextInput
+        placeholder="Username"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        style={styles.input}
+        secureTextEntry={true}
+      />
+      <Button title="Login" onPress={handleClick} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+  input: {
+    width: 300,
+    margin: 12,
+    paddingBottom: 3,
+    borderBottomWidth: 1,
   },
 });
